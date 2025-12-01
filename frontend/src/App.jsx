3b01cb02ea6201b41f399f9a41f5fa1452c84c95
nerbertb <nerbertb@gmail.com>
2025-12-01 13:08:31 +0800

    Fix: Vercel / Route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path="/" element={<Navigate to="/login" />} />
         <Route path="/login" element={<Login />} />
         <Route path="/home" element={<Home />} />
       </Routes>
